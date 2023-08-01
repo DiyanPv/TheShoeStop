3659e6f1ebc85a150ce13320359f0cb7e80337e4
DiyanPv <diyanpetrov61@gmail.com>
2023-08-01 19:34:09 +0300

    load more functionality implemented

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -8,15 +8,13 @@
 
 import React from "react";
 import "./ProductCard.css";
-const ProductCard = (imageUrl) => {
-  const dynamicImage =
-    "https://static.nike.com/a/images/t_default/4dec6834-9921-4a6b-a76f-e7735ed9326f/nikecourt-zoom-pro-mens-hard-court-tennis-shoes-c5mPwW.png";
+const ProductCard = ({ url, name, details, stars, price }) => {
   return (
     <div className="flex flex-col product-item-card relative xs:min-w-[100%] sm:min-w-[45%] sm:max-w-[45%] sm:min-h-[150px] md:min-w-[45%] md:max-w-[45%] md:min-h-[220px] lg:min-w-[23%] lg:min-h-[220px] lg:max-w-[30%] ">
       <div
         className="w-full h-full"
         style={{
-          backgroundImage: `url(${dynamicImage})`,
+          backgroundImage: `url(${url})`,
           backgroundSize: "100%",
           backgroundPosition: "center",
           backgroundRepeat: `no-repeat`,
@@ -25,12 +23,12 @@ const ProductCard = (imageUrl) => {
       <div className="flex-col text-xs py-2 px-2 w-full ">
         <div className="flex justify-between">
           <div>
-            <h2 className="font-bold">Nike Air Max</h2>
-            <p>Details</p>
-            <h2 className="font-medium">2,000 USD</h2>
+            <h2 className="font-bold">{name}</h2>
+            <p>{details}</p>
+            <h2 className="font-medium">${price}</h2>
           </div>
 
-          <div className="flex items-center">4.5/5</div>
+          <div className="flex items-center">{stars}/5</div>
         </div>
       </div>
     </div>
--- src/components/ProductContainer/ProductContainer.jsx
+++ src/components/ProductContainer/ProductContainer.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductContainer.css";
 import ProductCard from "../ProductCard/ProductCard";
+import shoes from "../../data/shoes.json";
 import { useSelector } from "react-redux";
-const ProductContainer = () => {
-  const { selectedSort, selectedFilter } = useSelector((state) => state.product)
 
-  console.log('selected sort => ', selectedSort.value);
-  console.log('selected filter => ', selectedFilter);
+const ProductContainer = () => {
+  const itemsPerPage = 8;
+  const loadMoreAmount = 4;
+  const [visibleAssets, setVisibleAssets] = useState(
+    shoes.women.slice(0, itemsPerPage)
+  );
+
+  const handleLoadMore = () => {
+    const nextIndex = visibleAssets.length;
+    const newVisibleAssets = shoes.women.slice(
+      nextIndex,
+      nextIndex + loadMoreAmount
+    );
+    setVisibleAssets((prevVisibleAssets) => [
+      ...prevVisibleAssets,
+      ...newVisibleAssets,
+    ]);
+  };
 
   return (
-    <div className="flex flex-wrap gap-6 xs:justify-center justify-between w-[100%] xl:border-l-2 xl:pl-6  ">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+    <div className="flex flex-wrap gap-6 xs:justify-center justify-between w-[100%] xl:border-l-2 xl:pl-6">
+      {visibleAssets.map((el, index) => (
+        <ProductCard
+          key={el.name + index}
+          url={el.image}
+          name={el.name}
+          details={el.description}
+          stars={el.stars}
+          price={el.price}
+        />
+      ))}
+      {visibleAssets.length <= shoes.women.length && (
+        <div
+          onClick={handleLoadMore}
+          className="w-full font-serif text-xl py-2 flex justify-center align-center"
+        >
+          <button className="border-2 px-4 py-2 rounded-md hover:bg-gray">Load More</button>
+        </div>
+      )}
     </div>
   );
 };
